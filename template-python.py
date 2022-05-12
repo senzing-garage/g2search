8fa4fb9201c1eebf84d4944ec39d8a3b63c93036
docktermj <michael@dockter.com>
2022-05-12 14:48:17 -0400

    #14 Migrate from master to main

--- template-python.py
+++ template-python.py
@@ -32,7 +32,7 @@ __version__ = "1.0.0"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2019-07-16'
 __updated__ = '2020-07-23'
 
-SENZING_PRODUCT_ID = "5xxx"  # See https://github.com/Senzing/knowledge-base/blob/master/lists/senzing-product-ids.md
+SENZING_PRODUCT_ID = "5xxx"  # See https://github.com/Senzing/knowledge-base/blob/main/lists/senzing-product-ids.md
 log_format = '%(asctime)s %(message)s'
 
 # Working with bytes.
